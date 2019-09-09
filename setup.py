0b26cd376ce940b27c7717446fbcb59630f32210
Florian Magin <florian.magin@sit.fraunhofer.de>
2019-09-09 18:56:26 +0200

    Fix setup.py to have a properly working package

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+
+from setuptools import setup, find_packages
+import os
 
 packages = \
 ['networkx-stubs']
 
 package_data = \
 {'': ['*']}
+
+
+
+def find_stub_files():
+    result = []
+    for root, dirs, files in os.walk('networkx-stubs'):
+        for file in files:
+            if file.endswith('.pyi'):
+                if os.path.sep in root:
+                    sub_root = root.split(os.path.sep, 1)[-1]
+                    file = os.path.join(sub_root, file)
+                result.append(file)
+    return result
 
 extras_require = \
 {':(python_version >= "2.7" and python_version < "2.8") or (python_version >= "3.3" and python_version < "3.5")': ['typing>=3.6,<4.0']}
@@ -15,7 +30,7 @@ setup_kwargs = {
     'version': '0.0.1',
     'author': 'Florian Magin',
     'packages': packages,
-    'package_data': package_data,
+    'package_data': {'networkx-stubs': find_stub_files()},
     'extras_require': extras_require,
     'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
 }
