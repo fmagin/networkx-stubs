99f931af1dcbb19b074b6706f2cb2bfd4e3e209e
Audrey Dutcher <audrey@rhelmot.io>
2023-05-12 17:33:23 +0800

    Improve a few things

--- networkx-stubs/classes/__init__.pyi
+++ networkx-stubs/classes/__init__.pyi
@@ -13,3 +13,11 @@ from .digraph import DiGraph as DiGraph
 from .graph import Graph as Graph
 from .multidigraph import MultiDiGraph as MultiDiGraph
 from .multigraph import MultiGraph as MultiGraph
+
+from . import ordered
+from . import function
+from . import digraph
+from . import graph
+from . import multidigraph
+from . import multigraph
+from . import reportviews
